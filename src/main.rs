cfdcff06bcaef4d5d39588c0402306b85721e680
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-10-10 18:16:12 -0400

    more ray

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,44 @@ fn vec_test(){
     println!("unit v1 {}", v1);
 }
 
+fn ray_test(){
+    let v1 = tracer::ray::vec3::Vec3 { e: [0.0,1.0,2.0] };
+    let v2 = tracer::ray::vec3::Vec3 { e: [10.0,11.0,12.0] };
+    let r1 = tracer::ray::Ray {a: v1, b: v2};
+    println!("{}", r1);
+}
+
+fn blue_shader(){
+    fn color(r1: ray){
+        
+    }
+    
+    let mut nx: u32 = 200;
+    let mut ny: u32 = 100;
+    
+    println!("P3\n{} {}\n255", nx, ny);
+    
+    let mut j = ny - 1;
+    while j >= 0 {
+        let mut i = 0;
+        while i < nx {
+            let r: f32 = i as f32/nx as f32;
+            let g: f32 = j as f32/ny as f32;
+            let b: f32 = 0.32;
+            let ir: u32 = (255.99*r) as u32;
+            let ig: u32 = (255.99*g) as u32;
+            let ib: u32 = (255.99*b) as u32;
+            println!("{} {} {}", ir, ig, ib);
+            // std::cout << ir << " " << ig << " " << ib << "\n";
+            i += 1;
+        }
+        j -= 1;
+    }
+}
+
 fn main() {
     // ppm_test();
-    vec_test();
+    // vec_test();
+    // ray_test();
+    
 }
--- src/tracer/ray.rs
+++ src/tracer/ray.rs
@@ -1,7 +1,34 @@
-#[path = "vec3.rs"]
-mod vec3;
+use std::fmt;
 
-struct Ray {
-    pub A: vec3::Vec3,
-    pub B: vec3::Vec3,
+#[path = "vec3.rs"]
+pub mod vec3;
+
+pub struct Ray {
+    pub a: vec3::Vec3,
+    pub b: vec3::Vec3,
+}
+
+impl Ray {
+    pub fn origin(&self) -> vec3::Vec3 {
+        self.a
+    }
+    
+    pub fn direction(&self) -> vec3::Vec3 {
+        self.b
+    }
+    
+    pub fn point_at_parameter(&self, t: f32) -> vec3::Vec3 {
+        self.a + self.b*t
+    }
+}
+
+impl fmt::Display for Ray {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "a: {} b:{}", self.a, self.b)
+    }
 }
